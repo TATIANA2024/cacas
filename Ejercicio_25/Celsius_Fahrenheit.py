084e755a3371b5bd39db8daf8d5e3a356c751e82
TATIANA2024 <78865126+TATIANA2024@users.noreply.github.com>
2021-06-03 19:27:23 -0500

    Add files via upload

---
+++ Ejercicio_25/Celsius_Fahrenheit.py
@@ -0,0 +1,14 @@
+#Write a Python program to convert temperatures to and from celsius,Fahrenheit
+
+
+def CELCIUS_FARENHEIT ():
+    valorCelsius  =  int ( input ( "Por favor ingrese el valor en celsius: " ))
+
+    valorFahrenheit  = ( valorCelsius  * 1.8 ) +  32
+
+    print( f'El valor en Fahrenheit es: { valorFahrenheit } ' )
+
+resultado = CELCIUS_FARENHEIT()
+
+
+
